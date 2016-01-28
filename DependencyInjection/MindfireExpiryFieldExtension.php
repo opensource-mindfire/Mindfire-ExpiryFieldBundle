133aeef893245311c55c0dbf1b4b36b92d78e4c1
Thomas Lallement <thomas.lallement@9online.fr>
2016-01-28 15:42:18 +0100

    Update MindfireExpiryFieldExtension.php

--- DependencyInjection/MindfireExpiryFieldExtension.php
+++ DependencyInjection/MindfireExpiryFieldExtension.php
@@ -9,6 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
